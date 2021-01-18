41857c9950e3473e32a855537f2b84f8b27a2956
DanyaalA <Danny13653@gmail.com>
2021-01-18 21:03:59 +0000

    Patch & Nop
    Added patch and Nop Functions

--- HackAnyGamePt2/proc.cpp
+++ HackAnyGamePt2/proc.cpp
@@ -1,10 +1,10 @@
-#include "windows.h"
+#include "stdafx.h"
 #include "proc.h"
 
-DWORD GetProcID(const wchar_t* procName) 
+DWORD GetProcId(const wchar_t* procName)
 {
 	DWORD procId = 0;
-	HANDLE hSnap = (CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0));
+	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 	if (hSnap != INVALID_HANDLE_VALUE)
 	{
 		PROCESSENTRY32 procEntry;
@@ -27,13 +27,13 @@ DWORD GetProcID(const wchar_t* procName)
 	return procId;
 }
 
-uintptr_t GetModuleBaseAddress(DWORD procID, const wchar_t* modName) 
+uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName)
 {
 	uintptr_t modBaseAddr = 0;
-	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procID);
+	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId);
 	if (hSnap != INVALID_HANDLE_VALUE)
 	{
-		MODULEENTRY32 modEntry; 
+		MODULEENTRY32 modEntry;
 		modEntry.dwSize = sizeof(modEntry);
 		if (Module32First(hSnap, &modEntry))
 		{
@@ -51,10 +51,10 @@ uintptr_t GetModuleBaseAddress(DWORD procID, const wchar_t* modName)
 	return modBaseAddr;
 }
 
-uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> offsets) 
+uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> offsets)
 {
-	uintptr_t addr = ptr; 
-	for (unsigned int i = 0; i < offsets.size(); i++)
+	uintptr_t addr = ptr;
+	for (unsigned int i = 0; i < offsets.size(); ++i)
 	{
 		ReadProcessMemory(hProc, (BYTE*)addr, &addr, sizeof(addr), 0);
 		addr += offsets[i];
